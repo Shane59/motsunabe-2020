0b276585bb75fad363e65bc377097973f21dc1ec
Shinya Aoi <saoi@Shinyas-MacBook-Pro.local>
2020-07-08 14:07:55 +0900

    first commit

---
+++ build/precache-manifest.313cb207a0a7b917612b1c14bc0b99cd.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e527278c06901eacb184fdfc7c307f15",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a84a3cb803e9bac397b5",
+    "url": "/static/css/main.c5788d77.chunk.css"
+  },
+  {
+    "revision": "d1a291bc3425ac9aba02",
+    "url": "/static/js/2.51b5da16.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.51b5da16.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a84a3cb803e9bac397b5",
+    "url": "/static/js/main.15a6a192.chunk.js"
+  },
+  {
+    "revision": "9195f286be2e220e09fc",
+    "url": "/static/js/runtime-main.85bf8cc6.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
